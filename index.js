00f1b27c2e9989d05afd07b229d71ab672e71dbf
Bharat Mhaskar <bmhaskar@gmail.com>
2016-03-16 21:15:36 +0530

    Fixed error in onError function where port was undefined. Removed user module include from index.js Added test for  checking is users are returned. Added npm script test, we can now run tests by `npm run test` command.

--- index.js
+++ index.js
@@ -5,22 +5,19 @@ const mongoose = require('mongoose');
 
 const config = require('./config/config');
 const middleware = require('./middlewares/middleware');
-const User = require('./models/user');
 
 mongoose.connect(config.database.mongoose);
-
 const app = express();
-
 app.use(middleware);
-
 const server = app.listen(config.port, config.host, function () {
     console.log('Book sharing application started at address: ' + server.address().address + ' port: ' + server.address().port);
 });
+
 const onError = function onError(error) {
     if (error.syscall !== 'listen') {
         throw error;
     }
-
+    const port = config.port;
     const bind = typeof port === 'string'
         ? 'Pipe ' + port
         : 'Port ' + port;
