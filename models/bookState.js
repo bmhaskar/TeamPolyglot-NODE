8a82d528652292196be7ac0edd2548e124798833
Bharat Mhaskar <bmhaskar@gmail.com>
2016-03-21 11:55:28 +0530

    Added BookState Model.

--- models/bookState.js
+++ models/bookState.js
@@ -1,3 +1,52 @@
+'use strict';
+
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 /**
- * Created by BharatM on 21-03-2016.
+ * @swagger
+ * definition:
+ *  NewBookState:
+ *    type: object
+ *    required:
+ *     - book
+ *     - uploadedBy
+ *    properties:
+ *      book:
+ *        $ref: '#/definition/Book'
+ *      uploadedBy:
+ *        $ref: '#/deffinition/ResponseUser'
+ *      uploadedBy:
+ *        $ref: '#/deffinition/ResponseUser'
+ *      lentBy:
+ *        $ref: '#/deffinition/ResponseUser'
+ *      lostBy:
+ *        $ref: '#/deffinition/ResponseUser'
+ *      returnedBy:
+ *        $ref: '#/deffinition/ResponseUser'
+ *      requestedBy:
+ *        type: array
+ *        items:
+ *          $ref: '#/deffinition/ResponseUser'
+ *  BookState:
+ *    allOf:
+ *      - $ref: '#/definitions/NewBookState'
+ *      - type: object
+ *        required:
+ *          _id:
+ *           type: string
+ *          createdAt:
+ *             type: dateTime
+ *          updatedAt:
+ *             type: dateTime
  */
+const bookStateSchema = new Schema({
+    book: {type: Schema.Types.ObjectId, ref: 'Book'},
+    uploadedBy: {type: Schema.Types.ObjectId, ref: 'User'},
+    lentBy: {type: Schema.Types.ObjectId, ref: 'User'},
+    returnedBy: {type: Schema.Types.ObjectId, ref: 'User'},
+    requestedBy: [{type: Schema.Types.ObjectId, ref: 'User'}],
+    lostBy: {type: Schema.Types.ObjectId, ref: 'User'}
+}, {timestamp: true});
+
+
+module.exports = mongoose.model('BookState', bookStateSchema);
